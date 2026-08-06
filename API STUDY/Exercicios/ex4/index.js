b2ff88203873f29c98666810ed674781d1063666
Davi Tavares <twitchdavi072@gmail.com>
2026-08-06 19:30:21 -0300

    .

--- API STUDY/Exercicios/ex4/index.js
+++ API STUDY/Exercicios/ex4/index.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { response } from "express";
 import bodyParser from "body-parser";
 import axios from "axios";
 
@@ -6,10 +6,27 @@ const app = express();
 const port = 3000;
 app.use(bodyParser.urlencoded());
 
-const API_URL = ""
-
-app.get("/", async (req,res)=>{
+app.get("/", async (req , res)=>{
+   
     try {
-        const result = await axios.get()
+        const result = await axios.get("https://dog.ceo/api/breeds/image/random");
+        res.render("index.ejs" , {content : JSON.stringify(result.data)});
+    } catch(error) {
+        res.render("index.ejs" , {error : "occured a error"});
     }
+})
+
+app.post("/raca", async (req,res)=>{
+    try {
+        const result = await axios.get(`https://dog.ceo/api/breed/${req.body.cachorro}/images/random`);
+        res.render("index.ejs" , {content : JSON.stringify(result.data)});
+    }catch(error){
+        res.render("index.ejs",{error : "ocurred a error"});
+    }
+})
+app.listen(port,(err)=> { 
+    if(err){
+        console.log("Ocurred a error");
+    }
+    console.log("Listening in port: 3000")
 })
