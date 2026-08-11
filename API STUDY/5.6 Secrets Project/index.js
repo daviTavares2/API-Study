81b3d1577b99629d19972db8ec004f78004d7723
Davi Tavares <twitchdavi072@gmail.com>
2026-08-10 21:23:47 -0300

    .

--- API STUDY/5.6 Secrets Project/index.js
+++ API STUDY/5.6 Secrets Project/index.js
@@ -11,3 +11,33 @@
 // secret and the username of the secret.
 
 // 6. Listen on your predefined port and start the server.
+
+import express from "express";
+import bodyParser, { urlencoded } from "body-parser";
+import axios from "axios";
+
+const app = express();
+const port = 4000;
+
+app.use(bodyParser.urlencoded({ extended:true }));
+app.use(express.static("public"));
+
+const API_URL = "https://secrets-api.appbrewery.com/random";
+
+app.get("/", async (res,req)=>{
+    res.render("index.ejs",)
+    try {
+        const result = axios.get(API_URL);
+        res.render("index.ejs" , {secret : JSON.stringify(result.data)});
+    } catch(error){
+        res.render("index.ejs" ,  { secret: JSON.stringify(error.response.data) });
+    }
+})
+
+
+app.listen(port , (err)=>{
+    if(err){
+        console.log("Occured a error")
+    }
+    console.log(`Listening on port : ${port}`);
+})
