8ce6f3c54b2c99c6120047a8f716871cca72f90d
Davi Tavares <twitchdavi072@gmail.com>
2026-08-14 19:48:43 -0300

    api study

--- API STUDY/6.0 DIY API/index.js
+++ API STUDY/6.0 DIY API/index.js
@@ -14,28 +14,74 @@ app.get("/random" , (req,res)=>{
 });
 
 //2. GET a specific joke
-app.get("/jokes/: id", async (req, res)=>{
-  const id = req.params.id;
-  try {
-    const response = await axios.get(`https://secrets-api.appbrewery.com/${id}`);
-    const result = response.data;
-    res.json(result);
-  } catch(error){
-    res.status(404);
-  }
+app.get("/jokes/:id" , (req , res)=>{
+  const id = parseInt(req.params.id);
+  const chosedJoke = jokes.find((joke)=> joke.id === id);
+  res.json(chosedJoke);
 });
 
 //3. GET a jokes by filtering on the joke type
-
+app.get("/filter", (req, res) => {
+  const type = req.query.type;
+  const filteredActivities = jokes.filter((joke) => joke.jokeType === type);
+  res.json(filteredActivities);
+});
 //4. POST a new joke
-
+app.post("/jokes", (req, res) => {
+  const newJoke = {
+    id : jokes.length + 1,
+    jokeText: req.body.text,
+    jokeType: req.body.type,
+  };
+  jokes.push(newJoke);
+  console.log(jokes.slice(-1));
+  res.json(newJoke);
+})
 //5. PUT a joke
+app.put("/jokes/:id", (req , res) =>{
+  const id = parseInt( req.params.id );
+  const replacementJoke = {
+    id : id,
+    jokeText : req.body.text,
+    jokeType: req.body.type,
+  };
 
+  const searchIndex = jokes.findIndex((joke)  => joke.id === id);
+
+  jokes[searchIndex] = replacementJoke;
+  res.json(replacementJoke);
+});
 //6. PATCH a joke
+app.patch("/jokes/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const existingJoke = jokes.find((joke) => joke.id === id);
+  const replacementJoke = {
+    id : id,
+    jokeText: req.body.text || existingJoke.jokeText,
+    jokeType: req.body.type || existingJoke.jokeType,
+  };
+  const searchIndex = jokes.findIndex((joke) => joke.id === id);
+  jokes[searchIndex] = replacementJoke;
+  console.log(jokes[searchIndex]);
+  res.json(replacementJoke);
+})
+
 
 //7. DELETE Specific joke
 
 //8. DELETE All jokes
+app.delete("/all", (req, res) => {
+  const userKey = req.query.key;
+  if (userKey === masterKey) {
+    jokes = [];
+    res.sendStatus(200);
+  }else {
+    res
+      .status(404)
+      .json({ error: `You are not authorised to perform this action.` });
+  }
+})
+
 
 app.listen(port, () => {
   console.log(`Successfully started server on port ${port}.`);
